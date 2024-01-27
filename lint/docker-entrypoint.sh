4a52bbd144863a9267f6b5089a89ecf8621404b2
Anonymo <zhengjackson12@hotmail.com>
2024-01-27 14:00:07 +1100

    check black diff

--- lint/docker-entrypoint.sh
+++ lint/docker-entrypoint.sh
@@ -6,7 +6,7 @@ for FILE_DIR in "$@"; do
 
     if [ "${CHECK_ONLY}" = "true" ];
     then
-        black "${FILE_DIR}" --check
+        black "${FILE_DIR}" --check --diff
         isort "${FILE_DIR}" --check-only
     else
         black "${FILE_DIR}"
